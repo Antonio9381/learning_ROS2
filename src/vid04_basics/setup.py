019d32e314f31dbe2a2750c174a62dfc730e8a23
Antonio938153 <awooo23@awutio23.localdomain>
2026-06-20 00:16:13 +1000

    Rename vid01_basics to vid04_basics

--- src/vid04_basics/setup.py
+++ src/vid04_basics/setup.py
@@ -5,7 +5,7 @@ import os
 from glob import glob
 #
 
-package_name = 'vid01_basics'
+package_name = 'vid04_basics'
 
 # This gets the directory where setup.py itself lives
 pkg_dir = os.path.dirname(os.path.abspath(__file__))
