fb3a65c61363f8be2a232d1027839f928d95a4d2
Antonio938153 <awooo23@awutio23.localdomain>
2026-06-22 00:40:56 +1000

    started writing vid07 and copied vid07 example to file system

--- src/vid07_describeRobotURDF/setup.py
+++ src/vid07_describeRobotURDF/setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'vid07_describeRobotURDF'
@@ -10,6 +11,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/description', glob('description/*')),
+        ('share/' + package_name + '/worlds', glob('worlds/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
